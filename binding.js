be09922960bac38299850a33da6cb8eca146542b
Razz Sunuwar <razzkoinch5@gmail.com>
2019-12-17 15:37:05 +0545

    First Commit

--- binding.js
+++ binding.js
@@ -1,20 +1,20 @@
 // Bind
-let demo = "";
-let c1 = {
-	x: 5,
-	y: 10
-};
+// let demo = "";
+// let c1 = {
+// 	x: 5,
+// 	y: 10
+// };
 
-let c2 = {
-	x: 75,
-	y: 235
-};
+// let c2 = {
+// 	x: 75,
+// 	y: 235
+// };
 
-function myWord() {
-	console.log(this.x + ','+ this.y);
-}
-let c1_fun = myWord.bind(c1);
-let c2_fun = myWord.bind(c2);
+// function myWord() {
+// 	console.log(this.x + ','+ this.y);
+// }
+// let c1_fun = myWord.bind(c1);
+// let c2_fun = myWord.bind(c2);
 
-c1_fun();
-c2_fun();
+// c1_fun();
+// c2_fun();
