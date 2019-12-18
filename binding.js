3a8a3c723dd77856574342a45a1532abf96d1503
Razz Sunuwar <razzkoinch5@gmail.com>
2019-12-18 10:10:33 +0545

    modified

--- binding.js
+++ binding.js
@@ -64,3 +64,54 @@
 
 // var x = person1.fullName.call(person2);
 // console.log(x);
+
+
+// Call
+
+// var obj = {num: 2};
+
+// var addToThis = function(a){
+// 	return this.num + a;
+// }
+// console.log(addToThis.call(obj, 3)); //functionname.call(obj, function arguments)
+
+
+
+// Apply
+
+// var obj = {num: 2};
+
+// var addToThis = function(a, b, c){
+// 	return this.num + a + b + c;
+// }
+// // console.log(addToThis.call(obj, 1, 2, 3));
+
+// var arr = [1 , 2, 3];
+// console.log(addToThis.apply(obj, arr));
+
+
+// Bind
+
+var obj = {num: 2};
+
+var addToThis = function(a, b, c){
+	return this.num + a + b + c;
+};
+
+var arr = [1 , 2, 3];
+
+var bound = addToThis.bind(obj);
+
+console.log(bound(1,2,3));
+
+
+
+
+
+
+
+
+
+
+
+
