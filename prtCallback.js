eb248e64ff7010687fee6f16575770fb00eb87ab
Razz Sunuwar <razzkoinch5@gmail.com>
2019-12-20 09:29:55 +0545

    modified

--- prtCallback.js
+++ prtCallback.js
@@ -29,8 +29,123 @@ complete some kind of routine or action.*/
 
 
 
-function shaw(callback){	//Arrow function
-	var a = 100;
-	callback(a);
+// function shaw(callback){	//Arrow function
+// 	var a = 100;
+// 	callback(a);
+// }
+// shaw(a => console.log("I am show Function "+ a));
+
+
+// function show(){
+// 	console.log('I am show function');
+// }
+// function shaw(callback){
+// 	callback();
+// }
+// shaw(show);
+// console.log("End");
+
+
+/* Synchronous - It waits for each operator to complete, after that
+it executes the next operation. Example given above*/
+
+
+// setTimeout(function show(){
+// 	console.log("I am show function");
+// }, 3000);
+// console.log("End");
+
+/*Asynchronous - It never waits for each operation to complete, rather it executes all operations
+in the first GO only. Example given above*/
+
+
+
+// function show(a){		//Callback function
+// 	console.log("I am show function " + a);
+
+// }
+
+// function task(callback){
+// 	var a = 5; //Assigning variable as a
+// 	callback(a); 
+// }
+// task(show); //Show is argument in task function
+
+/*						OR 							*/
+
+
+// function show(a){		//Callback function
+// 	console.log("I am show function " + a);
+
+// }
+
+// function task(a, callback){
+// 	// var a = 5; //Assigning variable as a
+// 	callback(a); 
+// }
+// task(5, show); //Show is argument in task function
+/*						OR 							*/
+
+
+
+// function task(callback){
+// 	var a = 5; //Assigning variable as a
+// 	callback(a); 
+// }
+// task(
+// 	function show(a){		//Callback function
+// 	console.log("I am show function " + a);
+// });
+/*						OR 							*/
+
+
+
+// function task(callback){
+// 	var a = 5; //Assigning variable as a
+// 	callback(a); 
+// }
+// task(
+// 	function(a){		//Callback function and Anonymous Functions
+
+// 	console.log("I am show function " + a);
+// });
+/*						OR 							*/
+
+
+
+// function task(callback){		//Arrow Function
+// 	var a = 5; //Assigning variable as a
+// 	callback(a); 
+// }
+
+// task(a => console.log("I am show function " + a));
+/*						OR 							*/
+
+
+
+
+// Synchronous
+
+function show(){
+	console.log('I am show function');
 }
-shaw(a => console.log("I am show Function "+ a));
+function shaw(callback){
+	callback();
+}
+shaw(show);
+console.log("End");
+
+
+// Asynchronous
+
+
+setTimeout(function show(){
+	console.log('I am show function');
+}, 3000);
+
+console.log("End");
+
+
+
+
+
