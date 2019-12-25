a765c2f4961ef3db5eff82117c1df3e234d63271
Razz Sunuwar <razzkoinch5@gmail.com>
2019-12-25 21:01:48 +0545

    initial commit

---
+++ dom.js
@@ -0,0 +1,21 @@
+/*JavaScript can access all the elements in a webpage making use of Document Object Model (DOM). In fact,
+the web browser creates a DOM of the webpage when the page is loaded.*/
+
+// FINDING HTML ELEMENTS
+
+/*Method:-
+document.getElementById(id)
+
+Description:
+Find an element by element id*/
+
+/*Method:-
+document.getElementsByTagName(name)
+
+Description: Find elements by tag name*/
+
+/*Method:- 
+document.getElementsByClassName(name)
+
+Description: Find elements by class name */
+
