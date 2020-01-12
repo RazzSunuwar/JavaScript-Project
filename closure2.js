0da4c2373d98108782873a7b1cab2175fc0ed5e1
RazzSunuwar <razzkoinch5@gmail.com>
2020-01-12 12:08:28 +0545

    Practice

--- closure2.js
+++ closure2.js
@@ -1,6 +1,6 @@
 // Tutorials practice
 
-// Closure are noth but function with preserve data
+// Closure are nothing but function with preserve data
 
 // var addTo = function (passed){
 //     var inner = 2;
@@ -30,21 +30,40 @@
 
 //          OR
 
-var addTo = function(passed){
-    var add = function(inner){
-        return passed + inner;
-    };
-    return add;
-};
+// var addTo = function(passed){
+//     var add = function(inner){
+//         return passed + inner;
+//     };
+//     return add;
+// };
 
-var addThree = new addTo(3);
-var addFour = new addTo(4);
+// var addThree = new addTo(3);
+// var addFour = new addTo(4);
 
-// console.dir(addThree);
-// console.dir(addFour);
+// // console.dir(addThree);
+// // console.dir(addFour);
 
-console.log(addThree(1));
-console.log(addFour(1));
+// console.log(addThree(1));
+// console.log(addFour(1));
 
 
- 
+// Closure is an inner function which has access to
+// 1 global function
+// 2 parent function argument
+// 3 parent function scope
+// 4 own argument
+// 5 own scope
+
+
+// Example
+// var welcome = "welcome";
+// function myName(name){
+//     let to = "to";
+//     function myPlace(place){     //myPlace is closure here.
+//         let abc = "abc";
+//         let text = welcome + " " + name + " " + to + " " + place;
+//         console.log("Text...", text);
+//     }
+//     myPlace("kathamandu");
+// }
+// myName("Ronald");
