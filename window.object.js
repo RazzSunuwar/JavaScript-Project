dba45edaa357951b3c074b8012675df0ca7e4f9c
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-05 09:51:08 +0545

    Window Object Practice

---
+++ window.object.js
@@ -0,0 +1,10 @@
+// Window object represents the browser's window or potentially frame, that a document is displayed
+// in.
+
+// As long as a browser window  is open, even if no document is loaded in the window, the window,
+// the window object is defined in the current model in memory.
+
+// All global JavaScript variable, function and objects automatically become members of the window
+// object.
+
+// Window => document
